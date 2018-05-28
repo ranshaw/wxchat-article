98ac0ddd2fed1d9d9875ffba08edd863f73ab238
tangyunxiao <tangyx@lybrc.com.cn>
2018-05-28 10:53:38 +0800

    修改代码

--- router/index.js
+++ router/index.js
@@ -1,27 +1,16 @@
-const Wechat = require('./wechat');
+'use strict';
 
-const API = new Wechat('493060037@qq.com', 'tang123,');
+const router = require('koa-router')();
+const common = require('./common/index');
+router.get('*', common);
 
-API.once('scan.login', (filepath) => {
-    // 登录二维码图片地址
-    console.log(filepath);
-});
 
-API.once('vcode', (filepath) => {
-    // 验证码图片地址
-    console.log(filepath);
-});
 
-API.login().then(data => {
-    console.log(data);
-    API.queryWxList('前端').then((e) => {
-        // console.log('搜索数据', e)
-    })
-    API.sendLoginUrl('需要确认登录22').then((v) => {
-        console.log('vvvvvv', v)
-    })
-}).catch(console.error.bind(console));
 
-API.qrdecode('./qrcode-login.png').then((v) => {
-    // console.log('二维码内容',v)
-})
+
+
+
+
+
+
+
